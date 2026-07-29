abc2d987e0d0f6434fcaf2a78546821427314d20
MoazEslam74 <moaz.eslam74@outlook.com>
2026-07-29 20:44:00 +0300

    Intial new model

--- lib/screens/users/delete.dart
+++ lib/screens/users/delete.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:bayt_alhikma_dashboard/utils/styles.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 class deleteUser extends StatefulWidget {
   const deleteUser({super.key});
 
