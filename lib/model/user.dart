d5ba21c33ec6cadb022e59ab65376b5f07dd9983
MoazEslam74 <moaz.eslam74@outlook.com>
2026-07-31 16:44:49 +0300

    End user model

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,7 +1,8 @@
 class User {
   final String username;
+  final String firstname;
+  final String lastname;
+  final String email;
 
-  User ({
-required this.username
-  });
+  User({required this.username, required this.firstname, required this.lastname, required this.email});
 }
